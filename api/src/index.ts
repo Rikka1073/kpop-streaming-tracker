69b74d4c1a930b70d48feecb3bd40cf0bf2a127a
Rikka1073 <masafumisoccer1073@yahoo.co.jp>
2026-01-28 22:03:53 +0900

    add:spotifyAPIの認証機能追加

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,26 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+type Bindings = {
+  SPOTIFY_CLIENT_ID: string;
+  SPOTIFY_CLIENT_SECRET: string;
+  SPOTIFY_REDIRECT_URL: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/login", (c) => {
+  const Params = new URLSearchParams({
+    response_type: "code",
+    client_id: c.env.SPOTIFY_CLIENT_ID,
+    scope: "user-read-private user-read-email",
+    redirect_uri: c.env.SPOTIFY_REDIRECT_URL,
+  });
+
+  return c.redirect(`https://accounts.spotify.com/authorize?${Params}`);
+});
+
+export default app;
